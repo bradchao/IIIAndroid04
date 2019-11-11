8c3177d643c4926a4aeeb3ec528c795690ee6f24
brad <brad@brad.tw>
2019-11-11 10:13:55 +0800

    3

--- app/src/main/java/tw/org/iii/iiiandroid04/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid04/MainActivity.java
@@ -3,6 +3,7 @@ package tw.org.iii.iiiandroid04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void exit(View view) {
+    }
+
+    public void setting(View view) {
+    }
+
+    public void newGame(View view) {
+    }
+
+    public void guess(View view) {
     }
 }
